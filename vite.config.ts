edfd1562794a981e586645c2a824b6f090ee6bf1
miaoSpring <539943419@qq.com>
2025-01-28 17:02:48 +0800

    fix: Spelling mistakes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
-import mkcert from'vite-plugin-mkcert'
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-    server: {
-    },
-    build: {
+  server: {
+  },
+  build: {
     lib: {
       entry: resolve(__dirname, './src/lib/jsQRGB.ts'),
       name: 'JsQRGB',
